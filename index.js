04e9aa8717e3e20af15c5204e94bae713f2bbdb2
mithun9421 <mithunmuralee94@gmail.com>
2021-10-15 22:39:58 +0530

    Fixed CORS issue

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 var express = require("express")
 var path = require("path")
-var app = express()
+var app = express();
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(express.static(path.join(__dirname, "dist")))
 
 let PORT = process.env.PORT || 4000
 const APIBASEURL = process.env.APIBASEURL || "/api"
-const WILDCARD = "*"
+const WILDCARD = "*";
 
 // Any api call will go thorough this
-app.get(APIBASEURL, (req, res) => res.send("Hello World from Express!"))
+app.get(APIBASEURL, (req, res) => res.json({ username: 'Flavio' }))
 
 // Index html served via express
 app.get(WILDCARD, function (req, res) {
-  console.log("Get *")
   res.sendFile("dist/index.html", { root: __dirname })
 })
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
+	componentDidMount() {
+		fetch("http://localhost:4000/api", {
+			crossDomain: true,
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json' },
+		})
+		.then(response => response.json())
+		.then(jsondata => console.log(jsondata))
+	}
 	render() {
 		return <div>Hello world f rom React.</div>; 
 	   } 
