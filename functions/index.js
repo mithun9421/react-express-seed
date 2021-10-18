376c0348631fbcecb763976fca505e29322d69e4
mithun9421 <mithunmuralee94@gmail.com>
2021-10-18 22:38:46 +0530

    Changed react build config

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,16 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	functions.logger.info('Hello logs!', { structuredData: true });
-	response.send('Hello from Firebase!');
+const app = express();
+app.use(
+	cors({
+		origin: true
+	})
+);
+
+app.get('/', (req, res) => {
+	res.send('Hello from express hosted in cloud functions');
 });
+
+exports.expressAPI = functions.https.onRequest(app);
